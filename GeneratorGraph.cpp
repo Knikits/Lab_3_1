4579212df51272e3c478c81b683373c9054c422c
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-06-29 19:24:40 +0300

    Init for generatorGraph

---
+++ GeneratorGraph.cpp
@@ -0,0 +1,18 @@
+#include "GeneratorGraph.h"
+
+GeneratorGraph::GeneratorGraph() {}
+
+QChartView* GeneratorGraph::GeneratorBar(QBarSeries * barS, bool color)
+{
+
+}
+
+QChartView* GeneratorGraph::GeneratorPie(QPieSeries * barS, bool color)
+{
+
+}
+
+void GeneratorGraph::PDF(QChartView *chView)
+{
+
+}
