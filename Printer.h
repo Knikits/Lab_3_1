df2b9a7d3b82416e8fa8418e69cb5df7b9d30b3b
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-06-29 19:07:41 +0300

    Create Printer class

--- Printer.h
+++ Printer.h
@@ -1,9 +1,23 @@
 #ifndef PRINTER_H
 #define PRINTER_H
 
+#include "DataGraph.h"
+#include <QtCharts>
+#include <QChartView>
+#include <QBarSet>
+#include <QBarSeries>
+#include <QPieSeries>
+
 class Printer
 {
-
+public:
+    DataGraph *date;
+    QBarSeries *bar;
+    QPieSeries *pie;
+    Printer();
+    virtual void getData (QMap<QString, int>) = 0;
+    QBarSeries* BarSeries() const {return bar;}
+    QPieSeries* PieSeries() const {return pie;}
 };
 
 #endif // PRINTER_H
