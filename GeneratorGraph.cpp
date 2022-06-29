2ceecd99f6fe519b574b9ed4a87eb43c2982c1a9
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-06-29 23:37:25 +0300

    color graph

--- GeneratorGraph.cpp
+++ GeneratorGraph.cpp
@@ -4,7 +4,23 @@ GeneratorGraph::GeneratorGraph() {}
 
 QChartView* GeneratorGraph::GeneratorBar(QBarSeries * barS, bool color)
 {
-
+    QChart* chart = new QChart();
+    if (color) // если график черно-белый
+    {
+        int j = 0;
+        for (auto set : barS->barSets())
+        {
+            set -> setColor(QColor(j,j,j));
+            j = j + 25;
+        }
+    }
+    else // если цветной график
+    {
+        for(auto set : barS->barSets())
+        {
+            set->setColor(QColor(qrand()%256, qrand()%256, qrand()%256));
+        }
+    }
 }
 
 QChartView* GeneratorGraph::GeneratorPie(QPieSeries * barS, bool color)
