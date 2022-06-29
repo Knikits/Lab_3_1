b05841548d965fea5157c7740b23050d769109f1
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-06-29 19:17:53 +0300

    Create GeneratorGraph class

--- GeneratorGraph.h
+++ GeneratorGraph.h
@@ -1,9 +1,20 @@
 #ifndef GENERATORGRAPH_H
 #define GENERATORGRAPH_H
 
+#include <QtCharts>
+#include <QChartView>
+#include <QBarSet>
+#include <QBarSeries>
+#include <QPieSeries>
+
 class GeneratorGraph
 {
-
+public:
+    GeneratorGraph();
+    QChartView *GeneratorBar(QBarSeries*, bool);
+    QChartView *GeneratorPie(QPieSeries*, bool);
+    void PDF(QChartView* chView);
+    //void JSON(QChartView* chView);
 };
 
 #endif // GENERATORGRAPH_H
